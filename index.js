187acac325047b54f7f515e0f23a3bec28340e45
raventeee <69177198+raventeee@users.noreply.github.com>
2022-06-17 04:52:01 +0800

    easier deployment
    made some env into variables

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function getPrices() {
 
 
 
-	axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${process.env.PREFERRED_CURRENCY}&ids=${process.env.COIN_ID}`).then(res => {
+	axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=cryptoblades-kingdoms`).then(res => {
 		if(res.data && res.data[0].current_price && res.data[0].price_change_percentage_24h) {
 			let currentPrice = res.data[0].current_price || 0 
 			let priceChange = res.data[0].price_change_percentage_24h || 0 
@@ -26,12 +26,12 @@ function getPrices() {
 					type: 3 
 				}
 			})
-			client.guilds.find(guild => guild.id === process.env.SERVER_ID1).me.setNickname(`$${(currentPrice).toLocaleString(undefined, {minimumFractionDigits:5}).replace(/,/g,process.env.THOUSAND_SEPARATOR)}${process.env.CURRENCY_SYMBOL}`)
-			client.guilds.find(guild => guild.id === process.env.SERVER_ID2).me.setNickname(`$${(currentPrice).toLocaleString(undefined, {minimumFractionDigits:5}).replace(/,/g,process.env.THOUSAND_SEPARATOR)}${process.env.CURRENCY_SYMBOL}`)
+			client.guilds.find(guild => guild.id === process.env.SERVER_ID1).me.setNickname(`$${(currentPrice).toLocaleString(undefined, {minimumFractionDigits:5}).replace(/,/g,",")}/KING`)
+			client.guilds.find(guild => guild.id === process.env.SERVER_ID2).me.setNickname(`$${(currentPrice).toLocaleString(undefined, {minimumFractionDigits:5}).replace(/,/g,",")}/KING`)
 			console.log('Updated price to', currentPrice)
 		}
 		else
-			console.log('Could not load player count data for', process.env.COIN_ID)
+			console.log('Could not load player count data for cryptoblades-kingdoms')
 
 	}).catch(err => console.log('Error at api.coingecko.com data:', err))
 }
@@ -41,7 +41,7 @@ client.on('ready', () => {
 	console.log('Logged in as', client.user.tag)
 
 	getPrices() 
-	setInterval(getPrices, Math.max(1, process.env.UPDATE_FREQUENCY || 1) * 60 * 1000)
+	setInterval(getPrices, Math.max(1, 5 || 1) * 60 * 1000)
 })
 
 client.login(process.env.DISCORD_TOKEN)
